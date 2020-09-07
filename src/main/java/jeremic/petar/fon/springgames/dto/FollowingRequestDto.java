8304708aa70c83ba294fe11472c4c5d47f2f3739
siux <jpetar765@gmail.com>
2020-09-07 11:00:13 +0200

    added FollowingRequestDto

---
+++ src/main/java/jeremic/petar/fon/springgames/dto/FollowingRequestDto.java
@@ -0,0 +1,22 @@
+package jeremic.petar.fon.springgames.dto;
+
+public class FollowingRequestDto {
+    private Long followerId;
+    private Long followingId;
+
+    public Long getFollowerId() {
+        return followerId;
+    }
+
+    public void setFollowerId(Long followerId) {
+        this.followerId = followerId;
+    }
+
+    public Long getFollowingId() {
+        return followingId;
+    }
+
+    public void setFollowingId(Long followingId) {
+        this.followingId = followingId;
+    }
+}
